238b7c651d460299d677f7b0015ee81bcb013deb
Hugh Kwon <83530270+Hugh-Kwong@users.noreply.github.com>
2023-05-03 19:39:18 -0400

    working model
    works at >70% accuracy

--- ML/data/digitdata/digitperceptron.py
+++ ML/data/digitdata/digitperceptron.py
@@ -1,37 +1,9 @@
 import numpy as np
 
-class Perceptron:
-    def __init__(self, input_size, learning_rate=0.1, num_epochs=100):
-        self.input_size = input_size
-        self.learning_rate = learning_rate
-        self.num_epochs = num_epochs
-        self.weights = np.zeros(49, 10)
-        
 
-    def predict(self, inputs):
-        inputs = np.hstack((inputs, [1]))
-        activation = np.dot(inputs, self.weights)
-        return 1 if activation >= 0 else 0
-
-    def train(self, train_data, train_labels):
-        for epoch in range(self.num_epochs):
-            for inputs, label in zip(train_data, train_labels):
-                inputs_flat = np.hstack(inputs)
-                prediction = self.predict(inputs_flat)
-                error = label - prediction
-                self.weights += self.learning_rate * error * np.hstack((inputs_flat, [1]))
-
-    def evaluate(self, test_data, test_labels):
-        correct = 0
-        for inputs, label in zip(test_data, test_labels):
-            inputs_flat = np.hstack(inputs)
-            prediction = self.predict(inputs_flat)
-            if prediction == label:
-                correct += 1
-        return correct / len(test_data)
 class SelfPerceptron:
     #weights will change to an array of 49 weights corresponding to each 4x4 split array look at comments on predict
-    def __init__(self, input_size = 27, learning_rate=0.1, num_epochs=1000):
+    def __init__(self, input_size = 27, learning_rate=0.1, num_epochs=2):
         self.input_size = input_size
         self.learning_rate = learning_rate
         self.num_epochs = num_epochs
@@ -48,9 +20,11 @@ class SelfPerceptron:
         epic = []
         for h in range(10):
             dot = []
-            for i in range(6):
-                for j in range(6):
-                    dot.append(sum(np.dot(self.weights[i][j],splitArr[i][j])))    
+            count = 0
+            for i in range(7):
+                for j in range(7):
+                    dot.append(sum(np.dot(self.weights[h][count],splitArr[i][j])))  
+                    count += 1  
             epic.append(sum(dot))
         temp2 = np.array(epic)
         return(temp2.argmax())
@@ -64,26 +38,38 @@ class SelfPerceptron:
         temp = digit28Array(training_data)
         ltemp = digitlabelArray(training_labels)
         #i is each array, 
-        for i in range(len(temp) ):
-            prediction = self.predict(temp[i])
-            # print(prediction, ltemp[i])
-            split = splitArray(temp[i])
-            # print(split[0][0])
-            if prediction !=  ltemp[i]:
-                count = 0
-                print("EPIC STYLE", prediction, ltemp[i])
-                for j in range(7):
-                    for k in range(7):
-                        self.weights[prediction][count] -= sum(split[j][k])
-                        print(self.weights[prediction][count])
-                        count += 1
-            
+        for z in range(2):
+            for i in range(len(temp) ):
+                prediction = self.predict(temp[i])
+                # print(prediction, ltemp[i])
+                split = splitArray(temp[i])
+                # print(split[0][0])
+                if prediction !=  ltemp[i]:
+                    count = 0
+                    label = ltemp[i]
+                    # print("EPIC STYLE", prediction, label)
+                    for j in range(7):
+                        for k in range(7):
+                            self.weights[prediction][count] -= sum(split[j][k])
+                            self.weights[int(label)][count] += sum(split[j][k])
+                            count += 1
+        # print(self.weights)
+                        
                 
+    def evaluate(self, testing_data, testing_labels):
+        temp = digit28Array(testing_data)
+        ltemp = digitlabelArray(testing_labels)
+        count = 0
+        for i in range(len(temp)):
+            prediction = self.predict(temp[i])
+            if(prediction == int(ltemp[i])):
+                count += 1
+        percentcorrect = count/len(temp)
+        print("Correct:", percentcorrect*100, "%")
+        return percentcorrect
                 
                     
-        #             self.weights[prediction][j] -= sum(split[prediction][j])
-        #             self.weights[ltemp[i]][j] += sum(split[ltemp[i]][j])
-        # print(self.weights)
+ 
         
             
     #reads in a digit image file and returns a list of 28x28 matrices with " "= 0, # = 1, and + = 2 WE CAN CHANGE THE + TO 1 AS WELL IF LAZY (this will matter much more in the prediction function,  this may change the %success rate)
@@ -135,36 +121,7 @@ def digitlabelArray(filename):
     return label_arr    
 
 
-def printArray(arr):
-    for i in range(len(arr)):
-        print()
-        for j in range(len(arr[i])):
-            print(arr[i][j])
-#transposes an array, doesnt work very well rn /
-#MIGHT NOT NEED
-def transposeArray(arr):
-    returnList = []
-    
-    for i in range(len(arr)):
-        x = np.array(arr[i])
-        x = x.transpose(1, 0)
-        returnList.append(x)
-    return returnList            
-# digits =printArray(digitArray("numbers.txt"),(digitlabelArray("numlab.txt"))   )
-# X = digitArray("numbers.txt")
 
-# X =transposeArray(X)
-# Y = digitlabelArray("numlab.txt")
-X = digit28Array("numbers.txt")
-L = np.zeros((10, 49))
-L = L
-Y = np.array(X[0])
-Y = np.hsplit(Y, 7)
-
-# for i in range(len(X)):
-#     print(i)
-# print(L[0])
-M = splitArray(X[0])
-# print(M[0][1])
 digitP = SelfPerceptron()
-digitP.train("numbers.txt", "numlab.txt")
+digitP.train("trainingimages.txt", "traininglabels.txt")
+digitP.evaluate("validationimages.txt", "validationlabels.txt")
