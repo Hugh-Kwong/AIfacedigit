627e1adb8593b6e1870f948c981ec3b96562de6d
Hugh Kwon <83530270+Hugh-Kwong@users.noreply.github.com>
2023-05-03 22:44:09 -0400

    Add files via upload

---
+++ ML/data/facedata/facenetwork.py
@@ -0,0 +1,58 @@
+import tensorflow as tf
+import numpy as np
+
+def faceArray(filename):
+    segment_size = 70
+    segments = []
+    with open(filename, 'r') as f:
+        current_segment = []
+        for i, line in enumerate(f):
+            segment_num = i // segment_size
+            if i % segment_size == 0:
+                if current_segment:
+                    segments.append(current_segment)
+                current_segment = []
+            current_segment.append([1 if ch == "#" else 0 for ch in line.rstrip("\n")])
+        if current_segment:
+            segments.append(current_segment)        
+    return segments
+
+
+#reads in a file containing the labels of the corresponding images [1,2,...]
+def facelabelArray(filename):
+    with open(filename) as file:
+        lines = file.readlines()
+    label_arr = []
+    for line in lines:
+        label_arr.append(line.rstrip("\n"))
+    return label_arr    
+
+# Load the MNIST dataset
+X = faceArray("trainingimages.txt")
+Y = facelabelArray("traininglabels.txt")
+X = np.array(X)
+Y = np.array(Y)
+test = faceArray("testimages.txt")
+ltest = facelabelArray("testlabels.txt") 
+test = np.array(test)
+ltest = np.array(ltest)
+
+X, test = X / 2, test / 2
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10)
+])
+
+# Compile the model
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+model.compile(optimizer='adam', loss=loss_fn, metrics=['accuracy'])
+
+# Train the model
+model.fit(X, Y, epochs=5)
+
+# Evaluate the model
+model.evaluate(test, ltest, verbose=2)
+
